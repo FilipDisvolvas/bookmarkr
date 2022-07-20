a655ae7d4af1fdc9132f5fb30ad708cfac95e617
Filip Sakrak <g@acts-as-coder.de>
2022-07-20 02:19:54 +0200

    Smoke Test hinzugefügt

---
+++ src/test/kotlin/net/sakrak/reactivebookmarks/ReactiveBookmarksApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.sakrak.reactivebookmarks
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveBookmarksApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
